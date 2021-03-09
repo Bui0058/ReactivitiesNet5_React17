998e6b6ca3f4789e51f6c0c48f5f889eef71bf6e
bui0058 <duongbv79@yahoo.com>
2021-03-09 12:07:10 +1030

    end of section 10

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,5 +1,8 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { toast } from 'react-toastify';
+import { history } from '../..';
 import { Activity } from '../models/activity';
+import { store } from '../stores/stores';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -8,13 +11,41 @@ const sleep = (delay: number) => {
 }
 
 axios.interceptors.response.use(async response =>  {
-    try {
         await sleep(1000);
-        return response;
-    } catch (error) {
-        console.log(error);
-        return await Promise.reject(error);
-    }
+        return response;    
+ }, (error: AxiosError) => {
+     const {data, status, config} = error.response!;   
+     
+     switch (status)  {        
+        case 400:
+            if (typeof data === 'string') { //this is the case of Bad Request
+                toast.error(data);
+            }
+            if (config.method === 'get' && data.errors.hasOwnProperty('id')) { // case of invalid Guid
+                history.push('/not-found');
+            }
+            if (data.errors) { //this is the case of Validation errors
+                const modalStateErrors = [];
+                for (const key in data.errors) {
+                    if (data.errors[key]) {
+                        modalStateErrors.push(data.errors[key]);
+                    }                    
+                }
+                throw modalStateErrors.flat();
+            } 
+            break;
+        case 401: 
+            toast.error("unauthorised");
+            break;
+        case 404: 
+            history.push('/not-found');
+            break;
+        case 500:
+            store.commonStore.setServerError(data);
+            history.push('/server-error');
+            break;
+     }
+     return Promise.reject(error);
  })
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,30 +3,40 @@ import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { observer } from "mobx-react-lite";
-import { Route, useLocation } from "react-router";
+import { Route, Switch, useLocation } from "react-router";
 import HomePage from "../../features/home/HomePage";
 import ActivityForm from "../../features/activities/forms/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import TestError from "../../features/errors/TestError";
+import NotFound from "../../features/errors/NotFound";
+import ServerError from '../../features/errors/ServerError';
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const location = useLocation();
 
   return (
     <>
+      <ToastContainer position="bottom-right" hideProgressBar />
       <Route exact path="/" component={HomePage} />
       <Route
-        path={'/(.+)'}
+        path={"/(.+)"}
         render={() => (
           <>
             <NavBar />
             <Container style={{ marginTop: "7em" }}>
-              <Route exact path="/activities" component={ActivityDashboard} />
-              <Route path="/activities/:id" component={ActivityDetails} />
-              <Route
-                key={location.key}
-                path={["/createActivity", "/manage/:id"]}
-                component={ActivityForm}
-              />
+              <Switch>
+                <Route exact path="/activities" component={ActivityDashboard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={["/createActivity", "/manage/:id"]}
+                  component={ActivityForm}
+                />
+                <Route path="/errors" component={TestError} />
+                <Route path="/server-error" component={ServerError} />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </>
         )}
