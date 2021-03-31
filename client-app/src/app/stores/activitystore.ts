f781e3d193439aacffec026283371a2d97ef1272
bui0058 <duongbv79@yahoo.com>
2021-04-01 10:19:08 +1030

    end of section 15

--- client-app/src/app/stores/activitystore.ts
+++ client-app/src/app/stores/activitystore.ts
@@ -1,7 +1,9 @@
 import {  makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
-import { Activity } from "../models/activity";
+import { Activity, ActivityFormValues } from "../models/activity";
 import {format} from 'date-fns';
+import { store } from "./stores";
+import { Profile } from "../models/profile";
 
 
 export default class ActivityStore {
@@ -68,6 +70,14 @@ export default class ActivityStore {
   }
 
   private setActivity = (activity: Activity) => {
+    const user = store.userStore.user;
+    if (user) {
+      activity.isGoing = activity.attendees!.some(
+        a => a.username === user.userName
+      );
+      activity.isHost = activity.hostUsername === user.userName;
+      activity.host = activity.attendees!.find(x => x.username === activity.hostUsername);
+    }
     activity.date = new Date(activity.date!);
     this.activityRegistry.set(activity.id, activity);
   }
@@ -81,15 +91,17 @@ export default class ActivityStore {
   }; // this is a second  approach instead of runInActin() while updating an observalbe inside another action. 
   // by creating the own action of the behavior need to update. 
 
-   createActivity = async (activity: Activity) => {
-      this.loading = true;
+   createActivity = async (activity: ActivityFormValues) => {
+      const user = store.userStore.user;
+      const attendee = new Profile(user!);
       try {
           await agent.Activities.create(activity);
+          const newActivity = new Activity(activity);
+          newActivity.hostUsername = user!.userName;
+          newActivity.attendees = [attendee];
+          this.setActivity(newActivity);
           runInAction(() => {
-              this.activityRegistry.set(activity.id, activity);
-              this.selectedActivity = activity;
-              this.editMode = false;
-              this.loading = false;
+              this.selectedActivity = newActivity;
           })
 
       } catch (error) {
@@ -100,15 +112,16 @@ export default class ActivityStore {
       }
   }
 
-  updateActivity = async (activity: Activity) => {
-      this.loading = true;
+  updateActivity = async (activity: ActivityFormValues) => {
       try {
           await agent.Activities.update(activity);
           runInAction(() => {
-              this.activityRegistry.set(activity.id, activity);
-              this.selectedActivity = activity;
-              this.editMode = false;
-              this.loading = false;
+            if (activity.id) {
+              //using spread operator to overide the vlue of current activity with new activity
+              let updatedActivity = {...this.getActivity(activity.id), ...activity};
+              this.activityRegistry.set(activity.id, updatedActivity as Activity);
+              this.selectedActivity = updatedActivity as Activity;
+            }
           })
 
       } catch(error) {
@@ -136,4 +149,46 @@ export default class ActivityStore {
         });       
       }
   }
+
+  updateAttendance = async () => {
+    const user = store.userStore.user;
+    this.loading = true;
+    try{
+      await agent.Activities.attend(this.selectedActivity!.id);
+      runInAction(() => {
+        if (this.selectedActivity?.isGoing) { //remove profile from attendees
+          this.selectedActivity.attendees = this.selectedActivity.attendees?.filter(a =>
+                                               a.username !== user?.userName);
+          this.selectedActivity.isGoing = false;
+        } else { //create a Profile then add to attendees of activity
+          const attendee = new Profile(user!);
+          this.selectedActivity?.attendees?.push(attendee);
+          this.selectedActivity!.isGoing = true;
+        }        
+        this.activityRegistry.set(this.selectedActivity!.id, this.selectedActivity!);
+      })
+    } catch (error) {
+      console.log(error);
+    } finally {
+      runInAction(() => this.loading = false);
+    }
+  }
+
+  cancelActivityToggle = async () => {
+    this.loading = true;
+    try{
+      await agent.Activities.attend(this.selectedActivity!.id);
+      runInAction(() => {
+        this.selectedActivity!.isCancelled = !this.selectedActivity?.isCancelled;
+        this.activityRegistry.set(this.selectedActivity!.id, this.selectedActivity!);
+      })
+    } catch (error) {
+      console.log(error);
+    }
+    finally{
+      runInAction(() => this.loading = false);
+    }
+    
+  }
+ 
 }
--- client-app/src/features/activities/forms/ActivityForm.tsx
+++ client-app/src/features/activities/forms/ActivityForm.tsx
@@ -13,7 +13,7 @@ import MyTextArea from '../../../app/common/form/MyTextArea';
 import MySelectInput from '../../../app/common/form/MySelectInput';
 import MyDateInput from '../../../app/common/form/MyDateInput';
 import {categoryOptions} from '../../../app/common/options/categoryOptions';
-import { Activity } from "../../../app/models/activity";
+import { ActivityFormValues } from "../../../app/models/activity";
 
 function ActivityForm() {
   const history = useHistory();
@@ -21,21 +21,12 @@ function ActivityForm() {
   const {
     createActivity,
     updateActivity,
-    loading,
     loadActivity,
     loadingInitial,
   } = activityStore;
   const { id } = useParams<{ id: string }>();
 
-  const [activity, setActivity] = useState<Activity>({
-    id: "",
-    title: "",
-    category: "",
-    description: "",
-    date: null,
-    city: "",
-    venue: "",
-  });
+  const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
   const validationSchema = Yup.object({
     title: Yup.string().required('The activity Title is required'),
@@ -47,11 +38,11 @@ function ActivityForm() {
   })
 
   useEffect(() => {
-    if (id) loadActivity(id).then((activity) => setActivity(activity!));
+    if (id) loadActivity(id).then((activity) => setActivity(new ActivityFormValues(activity)));
   }, [id, loadActivity]);
 
-  function handleFormSubmit(activity: Activity) {
-    if (activity.id.length === 0) {
+  function handleFormSubmit(activity: ActivityFormValues) {
+    if (!activity.id) {
       let newActivity = {
         ...activity,
         id: uuid(),
@@ -96,7 +87,7 @@ function ActivityForm() {
             <MyTextInput placeholder="Venue" name="venue" />
             <Button
               disabled={isSubmitting || !dirty || !isValid}
-              loading={loading}
+              loading={isSubmitting}
               floated="right"
               positive
               type="submit"
